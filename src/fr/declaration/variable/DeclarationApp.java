eb84be5fe2e12b3c61381ab8b5977adf817e417b
Soliquidus <tibo.pfeifer@gmail.com>
2021-10-04 12:31:05 +0200

    TP03 - Fin

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,24 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		
+		byte exempleByte = 1;
+		short exempleShort = 20;
+		int exempleInt = 300;
+		long exempleLong = 3000;
+		float exempleFloat = 4.7F;
+		double exempleDouble = 03.04D;
+		char exempleChar = 'C';
+		boolean exempleBooleen = true;
+
+		//Affichage de la valeur d'une variable
+		System.out.println(exempleDouble + "et" + exempleByte);
+
+		//Retour à la ligne d'une chaîne de charactère
+		String randomString = "Voici le résultat d'un calcul :\n 1+5=6";
+		System.out.println(randomString);
+	}
+
+}
